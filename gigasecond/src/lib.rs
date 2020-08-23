ff4d3554ce040210268969d9670c141fa44b0c07
Paul Mabileau <paulmabileau@hotmail.fr>
2020-08-23 17:38:00 +0200

    Core: Gigasecond
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

---
+++ gigasecond/src/lib.rs
@@ -0,0 +1,7 @@
+use chrono::{DateTime, Utc};
+use time::Duration;
+
+/// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start + Duration::seconds(1_000_000_000)
+}
