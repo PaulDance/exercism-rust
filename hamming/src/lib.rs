e5616b4a398c984744a9ecf7ff91f57fcc9ba4ef
Paul Mabileau <paulmabileau@hotmail.fr>
2020-09-15 16:37:49 +0200

    Side: Hamming
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

---
+++ hamming/src/lib.rs
@@ -0,0 +1,13 @@
+/// Returns the Hamming distance between the strings, or None if the lengths are mismatched.
+pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
+    if s1.len() != s2.len() {
+        None
+    } else {
+        Some(
+            s1.chars()
+                .zip(s2.chars())
+                .filter(|(c1, c2)| c1 != c2)
+                .count(),
+        )
+    }
+}
