f301461229c120f69dfb24c77ff1c8fbe24ad849
Paul Mabileau <paulmabileau@hotmail.fr>
2023-08-21 20:11:39 +0200

    Update: Armstrong Numbers
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,8 +1,8 @@
-pub fn is_armstrong_number(num: u32) -> bool {
+pub fn is_armstrong_number(num: u64) -> bool {
     let num_str = num.to_string();
     num_str
         .chars()
-        .map(|chr| chr.to_digit(10).unwrap().pow(num_str.len() as u32))
-        .sum::<u32>()
+        .map(|chr| (chr.to_digit(10).unwrap() as u64).pow(num_str.len() as u32))
+        .sum::<u64>()
         == num
 }
