e7b653096e8467173bdf1c91da5893fa9f759570
Paul Mabileau <paulmabileau@hotmail.fr>
2023-08-21 21:48:22 +0200

    Update: Robot Name
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- robot-name/tests/robot-name.rs
+++ robot-name/tests/robot-name.rs
@@ -3,11 +3,11 @@ use robot_name as robot;
 fn assert_name_matches_pattern(n: &str) {
     assert!(n.len() == 5, "name is exactly 5 characters long");
     assert!(
-        n[0..2].chars().all(|c| c >= 'A' && c <= 'Z'),
+        n[0..2].chars().all(|c| c.is_ascii_uppercase()),
         "name starts with 2 uppercase letters"
     );
     assert!(
-        n[2..].chars().all(|c| c >= '0' && c <= '9'),
+        n[2..].chars().all(|c| c.is_ascii_digit()),
         "name ends with 3 numbers"
     );
 }
@@ -37,6 +37,18 @@ fn test_different_robots_have_different_names() {
     let r1 = robot::Robot::new();
     let r2 = robot::Robot::new();
     assert_ne!(r1.name(), r2.name(), "Robot names should be different");
+}
+
+#[test]
+fn test_many_different_robots_have_different_names() {
+    use std::collections::HashSet;
+
+    // In 3,529 random robot names, there is ~99.99% chance of a name collision
+    let vec: Vec<_> = (0..3529).map(|_| robot::Robot::new()).collect();
+    let set: HashSet<_> = vec.iter().map(|robot| robot.name()).collect();
+
+    let number_of_collisions = vec.len() - set.len();
+    assert_eq!(number_of_collisions, 0);
 }
 
 #[test]
