178b1a44cb6a24366a0a2b03e61c460bdc14f2ce
Paul Mabileau <paulmabileau@hotmail.fr>
2024-08-27 19:53:49 +0200

    Update: Decimal
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- decimal/README.md
+++ decimal/README.md
@@ -2,6 +2,7 @@
 
 Welcome to Decimal on Exercism's Rust Track.
 If you need help running the tests or submitting your code, check out `HELP.md`.
+If you get stuck on the exercise, check out `HINTS.md`, but try and solve it without using those first :)
 
 ## Instructions
 
@@ -16,10 +17,6 @@ Despite `Decimal` being a custom type, we should still be able to treat them as 
 In Rust, the way to get these operations on custom types is to implement the relevant traits for your custom object. In particular, you'll need to implement at least `PartialEq`, `PartialOrd`, `Add`, `Sub`, and `Mul`. Strictly speaking, given that the decimal numbers form a total ordering, you should also implement `Eq` and `Ord`, though those traits are not checked for by these tests.
 
 It would be very easy to implement this exercise by using the [bigdecimal](https://crates.io/crates/bigdecimal) crate. Don't do that; implement this yourself.
-
-- Instead of implementing arbitrary-precision arithmetic from scratch, consider building your type on top of the [num_bigint](https://crates.io/crates/num-bigint) crate.
-- You might be able to [derive](https://doc.rust-lang.org/book/2018-edition/appendix-03-derivable-traits.html) some of the required traits.
-- `Decimal` is assumed to be a signed type. You do not have to create a separate unsigned type, though you may do so as an implementation detail if you so choose.
 
 ## Source
 
--- decimal/tests/decimal.rs
+++ decimal/tests/decimal.rs
@@ -16,7 +16,7 @@ const BIGS: [&str; 3] = [
 
 // test simple properties of required operations
 #[test]
-fn test_eq() {
+fn eq() {
     assert!(decimal("0.0") == decimal("0.0"));
     assert!(decimal("1.0") == decimal("1.0"));
     for big in BIGS.iter() {
@@ -25,40 +25,40 @@ fn test_eq() {
 }
 
 #[test]
-fn test_ne() {
+fn ne() {
     assert!(decimal("0.0") != decimal("1.0"));
     assert!(decimal(BIGS[0]) != decimal(BIGS[1]));
 }
 
 #[test]
-fn test_gt() {
+fn gt() {
     for slice_2 in BIGS.windows(2) {
         assert!(decimal(slice_2[1]) > decimal(slice_2[0]));
     }
 }
 
 #[test]
-fn test_lt() {
+fn lt() {
     for slice_2 in BIGS.windows(2) {
         assert!(decimal(slice_2[0]) < decimal(slice_2[1]));
     }
 }
 
 #[test]
-fn test_add() {
+fn add() {
     assert_eq!(decimal("0.1") + decimal("0.2"), decimal("0.3"));
     assert_eq!(decimal(BIGS[0]) + decimal(BIGS[1]), decimal(BIGS[2]));
     assert_eq!(decimal(BIGS[1]) + decimal(BIGS[0]), decimal(BIGS[2]));
 }
 
 #[test]
-fn test_sub() {
+fn sub() {
     assert_eq!(decimal(BIGS[2]) - decimal(BIGS[1]), decimal(BIGS[0]));
     assert_eq!(decimal(BIGS[2]) - decimal(BIGS[0]), decimal(BIGS[1]));
 }
 
 #[test]
-fn test_mul() {
+fn mul() {
     for big in BIGS.iter() {
         assert_eq!(decimal(big) * decimal("2"), decimal(big) + decimal(big));
     }
@@ -66,7 +66,7 @@ fn test_mul() {
 
 // test identities
 #[test]
-fn test_add_id() {
+fn add_id() {
     assert_eq!(decimal("1.0") + decimal("0.0"), decimal("1.0"));
     assert_eq!(decimal("0.1") + decimal("0.0"), decimal("0.1"));
     assert_eq!(decimal("0.0") + decimal("1.0"), decimal("1.0"));
@@ -74,43 +74,53 @@ fn test_add_id() {
 }
 
 #[test]
-fn test_sub_id() {
+fn sub_id() {
     assert_eq!(decimal("1.0") - decimal("0.0"), decimal("1.0"));
     assert_eq!(decimal("0.1") - decimal("0.0"), decimal("0.1"));
 }
 
 #[test]
-fn test_mul_id() {
+fn mul_id() {
     assert_eq!(decimal("2.1") * decimal("1.0"), decimal("2.1"));
     assert_eq!(decimal("1.0") * decimal("2.1"), decimal("2.1"));
 }
 
 #[test]
-fn test_gt_positive_and_zero() {
+fn gt_positive_and_zero() {
     assert!(decimal("1.0") > decimal("0.0"));
     assert!(decimal("0.1") > decimal("0.0"));
 }
 
 #[test]
-fn test_gt_negative_and_zero() {
+fn gt_negative_and_zero() {
     assert!(decimal("0.0") > decimal("-0.1"));
     assert!(decimal("0.0") > decimal("-1.0"));
 }
 
+#[test]
+fn unequal_number_of_decimal_places() {
+    assert!(decimal("3.14") > decimal("3.13"));
+    assert!(decimal("3.14") > decimal("3.131"));
+    assert!(decimal("3.14") > decimal("3.1"));
+    assert!(decimal("3.13") < decimal("3.14"));
+    assert!(decimal("3.131") < decimal("3.14"));
+    assert!(decimal("3.1") < decimal("3.14"));
+}
+
 // tests of arbitrary precision behavior
 #[test]
-fn test_add_uneven_position() {
+fn add_uneven_position() {
     assert_eq!(decimal("0.1") + decimal("0.02"), decimal("0.12"));
 }
 
 #[test]
-fn test_eq_vary_sig_digits() {
+fn eq_vary_sig_digits() {
     assert!(decimal("0") == decimal("0000000000000.0000000000000000000000"));
     assert!(decimal("1") == decimal("00000000000000001.000000000000000000"));
 }
 
 #[test]
-fn test_add_vary_precision() {
+fn add_vary_precision() {
     assert_eq!(
         decimal("100000000000000000000000000000000000000000000")
             + decimal("0.00000000000000000000000000000000000000001"),
@@ -119,7 +129,7 @@ fn test_add_vary_precision() {
 }
 
 #[test]
-fn test_cleanup_precision() {
+fn cleanup_precision() {
     assert_eq!(
         decimal("10000000000000000000000000000000000000000000000.999999999999999999999999998",)
             + decimal(
@@ -130,7 +140,7 @@ fn test_cleanup_precision() {
 }
 
 #[test]
-fn test_gt_varying_positive_precisions() {
+fn gt_varying_positive_precisions() {
     assert!(decimal("1.1") > decimal("1.01"));
     assert!(decimal("1.01") > decimal("1.0"));
     assert!(decimal("1.0") > decimal("0.1"));
@@ -138,14 +148,14 @@ fn test_gt_varying_positive_precisions() {
 }
 
 #[test]
-fn test_gt_positive_and_negative() {
+fn gt_positive_and_negative() {
     assert!(decimal("1.0") > decimal("-1.0"));
     assert!(decimal("1.1") > decimal("-1.1"));
     assert!(decimal("0.1") > decimal("-0.1"));
 }
 
 #[test]
-fn test_gt_varying_negative_precisions() {
+fn gt_varying_negative_precisions() {
     assert!(decimal("-0.01") > decimal("-0.1"));
     assert!(decimal("-0.1") > decimal("-1.0"));
     assert!(decimal("-1.0") > decimal("-1.01"));
@@ -154,26 +164,26 @@ fn test_gt_varying_negative_precisions() {
 
 // test signed properties
 #[test]
-fn test_negatives() {
+fn negatives() {
     assert!(Decimal::try_from("-1").is_some());
     assert_eq!(decimal("0") - decimal("1"), decimal("-1"));
     assert_eq!(decimal("5.5") + decimal("-6.5"), decimal("-1"));
 }
 
 #[test]
-fn test_explicit_positive() {
+fn explicit_positive() {
     assert_eq!(decimal("+1"), decimal("1"));
     assert_eq!(decimal("+2.0") - decimal("-0002.0"), decimal("4"));
 }
 
 #[test]
-fn test_multiply_by_negative() {
+fn multiply_by_negative() {
     assert_eq!(decimal("5") * decimal("-0.2"), decimal("-1"));
     assert_eq!(decimal("-20") * decimal("-0.2"), decimal("4"));
 }
 
 #[test]
-fn test_simple_partial_cmp() {
+fn simple_partial_cmp() {
     assert!(decimal("1.0") < decimal("1.1"));
     assert!(decimal("0.00000000000000000000001") > decimal("-20000000000000000000000000000"));
 }
@@ -182,103 +192,103 @@ fn test_simple_partial_cmp() {
 // these tests are designed to ensure correctness of implementations for which the
 // integer and fractional parts of the number are stored separately
 #[test]
-fn test_carry_into_integer() {
+fn carry_into_integer() {
     assert_eq!(decimal("0.901") + decimal("0.1"), decimal("1.001"))
 }
 
 #[test]
-fn test_carry_into_fractional_with_digits_to_right() {
+fn carry_into_fractional_with_digits_to_right() {
     assert_eq!(decimal("0.0901") + decimal("0.01"), decimal("0.1001"))
 }
 
 #[test]
-fn test_add_carry_over_negative() {
+fn add_carry_over_negative() {
     assert_eq!(decimal("-1.99") + decimal("-0.01"), decimal("-2.0"))
 }
 
 #[test]
-fn test_sub_carry_over_negative() {
+fn sub_carry_over_negative() {
     assert_eq!(decimal("-1.99") - decimal("0.01"), decimal("-2.0"))
 }
 
 #[test]
-fn test_add_carry_over_negative_with_fractional() {
+fn add_carry_over_negative_with_fractional() {
     assert_eq!(decimal("-1.99") + decimal("-0.02"), decimal("-2.01"))
 }
 
 #[test]
-fn test_sub_carry_over_negative_with_fractional() {
+fn sub_carry_over_negative_with_fractional() {
     assert_eq!(decimal("-1.99") - decimal("0.02"), decimal("-2.01"))
 }
 
 #[test]
-fn test_carry_from_rightmost_one() {
+fn carry_from_rightmost_one() {
     assert_eq!(decimal("0.09") + decimal("0.01"), decimal("0.1"))
 }
 
 #[test]
-fn test_carry_from_rightmost_more() {
+fn carry_from_rightmost_more() {
     assert_eq!(decimal("0.099") + decimal("0.001"), decimal("0.1"))
 }
 
 #[test]
-fn test_carry_from_rightmost_into_integer() {
+fn carry_from_rightmost_into_integer() {
     assert_eq!(decimal("0.999") + decimal("0.001"), decimal("1.0"))
 }
 
 // test arithmetic borrow rules
 #[test]
-fn test_add_borrow() {
+fn add_borrow() {
     assert_eq!(decimal("0.01") + decimal("-0.0001"), decimal("0.0099"))
 }
 
 #[test]
-fn test_sub_borrow() {
+fn sub_borrow() {
     assert_eq!(decimal("0.01") - decimal("0.0001"), decimal("0.0099"))
 }
 
 #[test]
-fn test_add_borrow_integral() {
+fn add_borrow_integral() {
     assert_eq!(decimal("1.0") + decimal("-0.01"), decimal("0.99"))
 }
 
 #[test]
-fn test_sub_borrow_integral() {
+fn sub_borrow_integral() {
     assert_eq!(decimal("1.0") - decimal("0.01"), decimal("0.99"))
 }
 
 #[test]
-fn test_add_borrow_integral_zeroes() {
+fn add_borrow_integral_zeroes() {
     assert_eq!(decimal("1.0") + decimal("-0.99"), decimal("0.01"))
 }
 
 #[test]
-fn test_sub_borrow_integral_zeroes() {
+fn sub_borrow_integral_zeroes() {
     assert_eq!(decimal("1.0") - decimal("0.99"), decimal("0.01"))
 }
 
 #[test]
-fn test_borrow_from_negative() {
+fn borrow_from_negative() {
     assert_eq!(decimal("-1.0") + decimal("0.01"), decimal("-0.99"))
 }
 
 #[test]
-fn test_add_into_fewer_digits() {
+fn add_into_fewer_digits() {
     assert_eq!(decimal("0.011") + decimal("-0.001"), decimal("0.01"))
 }
 
 // misc tests of arithmetic properties
 #[test]
-fn test_sub_into_fewer_digits() {
+fn sub_into_fewer_digits() {
     assert_eq!(decimal("0.011") - decimal("0.001"), decimal("0.01"))
 }
 
 #[test]
-fn test_add_away_decimal() {
+fn add_away_decimal() {
     assert_eq!(decimal("1.1") + decimal("-0.1"), decimal("1.0"))
 }
 
 #[test]
-fn test_sub_away_decimal() {
+fn sub_away_decimal() {
     assert_eq!(decimal("1.1") - decimal("0.1"), decimal("1.0"))
 }
